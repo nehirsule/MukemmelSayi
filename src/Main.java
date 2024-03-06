fc7589105b807d95039819f240c9244e6e7e7c51
Şule Nehir ARAÇ <nehir.2706@gmail.com>
2024-03-06 13:09:25 +0300

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //Kullanıcıdan sayıyı iste ve kaydet
+        System.out.println("Bir Sayı Giriniz : ");
+        int n = input.nextInt();
+        int total = 0;
+
+
+        //1 den sayıya kadar olan tüm bölenleri belirle ve topla
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                total += i;
+            }
+        }
+
+        // belirlenip toplanan sayının tam bölenleri n sayısına eşit ise mükemmel; değilse değildir yazdır .
+        if (total == n) {
+            System.out.println(n + " Sayısı Mükemmel Sayıdır.");
+        } else {
+            System.out.println(n + "Sayısı Mükemmel Sayı Değildir .");
+        }
+    }
 }
